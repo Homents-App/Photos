55a5e52547aeeac23a5f52c7b19827d46e4fac1c
Daniel Lee <dsl8va@gmail.com>
2020-12-14 20:14:03 -0800

    edited api endpoints

--- server/app.js
+++ server/app.js
@@ -14,8 +14,13 @@ app.use(express.static(path.join(__dirname, '/../client/dist')));
 
 // Retrieves all listing data and photos for given id
 app.get('/api/listings/:id', (req, res) => {
+  function getRandomNum(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+  let fakeId = getRandomNum(8000000, 10000000);
+  let id = req.params.id || fakeId;
 
-  db.getListingData(req.params.id)
+  db.getListingData(id)
     .then(listing => {
       console.log('server here:', req.params.id);
       if (!listing) {throw new Error;}
@@ -29,30 +34,27 @@ app.get('/api/listings/:id', (req, res) => {
     .catch(err => {
       res.status(404).send(err);
     })
-
 })
-
 
 app.get('/loaderio-2d312fb6eb126eee0159e8c5bb0fd79a.txt', (req, res) => {
   res.sendFile(path.join(__dirname, '../loaderio.txt'))
 })
 
+// app.get('/', (req, res) => {
+//   function getRandomNum(min, max) {
+//     return Math.floor(Math.random() * (max - min)) + min;
+//   }
+//   let id = getRandomNum(8000000, 10000000);
 
-app.get('/', (req, res) => {
-  function getRandomNum(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-  let id = getRandomNum(8000000, 10000000);
-
-  db.getListingData(id)
-    .then(listing => {
-      if (!listing) {throw new Error;}
-      res.status(200).send(listing);
-    })
-    .catch(err => {
-      res.status(404).send(err);
-    })
-})
+//   db.getListingData(id)
+//     .then(listing => {
+//       if (!listing) {throw new Error;}
+//       res.status(200).send(listing);
+//     })
+//     .catch(err => {
+//       res.status(404).send(err);
+//     })
+// })
 
 // Creates listing with params1 and creates photos from params2
 app.post('/api/addListing', (req, res) => {
